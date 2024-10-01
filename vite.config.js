41ed4e4b96966463af6ed7b6b56342060978b0c0
sincemilkyway <97221197+heysouravv@users.noreply.github.com>
2024-10-01 14:21:31 +0530

    compressed images

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base:"https://coral-penguin-499874.hostingersite.com/",
+  // base:"https://coral-penguin-499874.hostingersite.com/",
   plugins: [react()],
   resolve: {
     alias: {
